e32c41de3370a86a17eef7248cf2ea246cd6d7ed
David Price <dprice@austindata.com>
2015-09-11 17:30:06 -0500

    Added specs to the project

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -3,25 +3,28 @@ import sbt.Keys._
 
 object Version {
   val scalaLogging = "3.1.0"
-  val logback = "1.1.3"
-  val config = "1.3.0"
-  val jaiImageio = "1.2-pre-dr-b04-2012-05-17"
+  val logback      = "1.1.3"
+  val config       = "1.3.0"
+  val jaiImageio   = "1.2-pre-dr-b04-2012-05-17"
+  val specs2       = "3.6.2"
 }
 
 object Library {
   val scalaLogging = "com.typesafe.scala-logging" 	%% "scala-logging" 	 % Version.scalaLogging
-  val logback = 	 "ch.qos.logback" 				% "logback-classic"  % Version.logback
-  val config = 		 "com.typesafe" 				% "config" 			 % Version.config
-  val jaiImageio =   "com.github.davenatx" 			% "jai-imageio-core" % Version.jaiImageio
-  }
+  val logback      = 	 "ch.qos.logback" 				% "logback-classic"  % Version.logback
+  val config       = 		 "com.typesafe" 				% "config" 			 % Version.config
+  val jaiImageio   =   "com.github.davenatx" 			% "jai-imageio-core" % Version.jaiImageio
+  val specs2       = "org.specs2"                 %% "specs2-core"    % Version.specs2
+}
 
 object Dependencies {
   import Library._
   
   val imageOps = List(
     scalaLogging,
-	logback,
-	config,
-	jaiImageio
+	 logback,
+	 config,
+	 jaiImageio,
+   specs2
   )
 }
